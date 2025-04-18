3e8d953094ad61f1f16fe69a2a20f4ed3ef81046
caarrp <junebirdsfly@gmail.com>
2025-04-18 12:54:56 -0700

    my computer is so cool it crashes all the time

--- vector.c
+++ vector.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include "vector.h"
+#include <math.h>
+
+double len(Vec *v_1){
+	//returns magnitude of 3d vector
+    double x_val = v_1->x;
+    double y_val = v_1->y;
+    double z_val = v_1->z;
+    
+    double mag = 
+}
--- vector.h
+++ vector.h
@@ -1,11 +1,16 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+
+
+
 void add(Vec *v_1, Vec *v_2);
 
 void dot(Vec *v_1, Vec *v_2);
 
 void cross(Vec *v_1, Vec *v_2);
+
+double len(Vec *v_1);
 
 
 
