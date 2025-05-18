92c33be1331cc4a5d95ee008327571ae58edeb5e
caarrp <junebirdsfly@gmail.com>
2025-05-17 23:09:06 -0700

    basic deeper updates

--- vector.h
+++ vector.h
@@ -8,11 +8,22 @@ typedef struct Vector{
     double x;
     double y; 
     double z;
+
+    double r;
+    double g;
+    double b;
+
+    double t;
 }Vec;
 
 Vec* malloc_v(double x, double y, double z);
 
-void add(Vec *v_1, Vec *v_2);
+Vec* add(Vec *v_1, Vec *v_2);
+Vec* sub(Vec *v_1, Vec *v_2);
+Vec* mult(Vec *v_1, Vec *v_2);
+Vec* div(Vec *v_1, Vec *v_2a);
+
+Vec* make_unit(Vec *v);
 
 void dot(Vec *v_1, Vec *v_2);
 
